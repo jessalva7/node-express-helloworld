6795be6d30e7fecbfee1d33695fdda765b92dd60
Jessal <jessal.va@outlook.com>
2020-10-29 00:52:18 +0530

    fixed Dockerfile and added docker-compose file

--- src/utilities/connection.js
+++ src/utilities/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
 module.exports = new Sequelize( 'pgDB' , 'postgres', 'password', {
-    host: 'localhost',
+    host: 'postgres',
     dialect: 'postgres'
 })
